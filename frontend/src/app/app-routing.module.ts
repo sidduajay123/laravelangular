ad55bfe9d84e0f953779a062fc25413619acb5a6
sidduajay123 <sidduajay123@gmail.com>
2018-11-16 17:51:08 +0530

    angular configur

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent,
+  },
+  {
+    path:'signup',
+    component:SignupComponent,
+  },
+  {
+    path:'profile',
+    component:ProfileComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
