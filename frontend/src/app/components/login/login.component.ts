0a938bdf014ff36e7e61b4193c7a91b4ae6d009f
sidduajay123 <sidduajay123@gmail.com>
2018-11-22 09:17:13 +0530

    Login and Signup working with token key

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JarwisService } from 'src/app/Services/jarwis.service';
+import { TokenService } from 'src/app/Services/token.service';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +17,24 @@ export class LoginComponent implements OnInit {
     email:null,
     password:null
   };
-  constructor(private http:HttpClient) { }
+  constructor(
+    private Jarwis:JarwisService,
+    private Token:TokenService,
+    private router:Router,
+    private Auth:AuthService,
+    ) { }
 
   onSubmit(){
-   return this.http.post('http://localhost:81/wae/backend/api/login',this.form).subscribe(
-     data => console.log(data),
+   this.Jarwis.login(this.form).subscribe(
+     data => this.handleResponse(data),
      error => this.handleError(error)
    );
+  }
+
+  handleResponse(data){
+    this.Token.handle(data.access_token);
+    this.Auth.changedAuthStatus(true);
+    this.router.navigateByUrl('/profile');
   }
 
   handleError(error){
--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { JarwisService } from 'src/app/Services/jarwis.service';
+import { TokenService } from 'src/app/Services/token.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public error = [];
+  public form = {
+    email:null,
+    name:null,
+    password:null,
+    password_confirmation:null
+  }
+  constructor(
+    private Jarwis:JarwisService,
+    private Token:TokenService,
+    private router:Router
+    ) { }
+
+  onSubmit(){
+    this.Jarwis.signup(this.form).subscribe(
+      data => this.handleResponse(data),
+      error => this.handleError(error)
+    )
+  }
+
+  handleResponse(data){
+    this.Token.handle(data.access_token);
+    this.router.navigateByUrl('/profile');
+  }
+
+  handleError(error){
+    this.error = error.error.errors;
+  }
 
   ngOnInit() {
   }
